f48951992f036116e52ccf6ae2301e1b7c7fee46
elyonousimohamed <melyonousi@redimpactarabia.com>
2024-02-05 20:05:58 +0100

    update

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -4,7 +4,7 @@ import { TTicket } from '../models/Ticket'
 import Ticket from '../components/Ticket'
 
 const fetchTickets = async () => {
-  const res = await fetch(process.env.BASE_API + '/tickets', {
+  const res = await fetch(process.env.NEXT_PUBLIC_BASE_API + '/tickets', {
     next: {
       revalidate: 0,
     }
